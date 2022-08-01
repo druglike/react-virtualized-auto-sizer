21fa50c703f1b08578b46c3d07536308a8246f20
Marak <marak.squires@gmail.com>
2022-08-01 11:30:36 -0400

    adds types

---
+++ index.d.ts
@@ -0,0 +1,22 @@
+declare module 'react-virtualized-auto-sizer' {
+  import { CSSProperties } from 'react';
+
+  interface Size {
+    height: number;
+    width: number;
+  }
+
+  class AutoSizer extends React.Component<{
+    className?: string;
+    defaultHeight?: number;
+    defaultWidth?: number;
+    disableHeight?: boolean;
+    disableWidth?: boolean;
+    nonce?: string;
+    onResize?: (size: Size) => void;
+    style?: CSSProperties;
+    children: (size: Size) => React.ReactNode;
+  }> {}
+
+  export default AutoSizer;
+}
